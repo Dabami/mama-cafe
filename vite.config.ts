0808d06b376b3999f30c949b2bf52ed6e9287c45
Dani <5517483+Dabami@users.noreply.github.com>
2024-11-21 02:43:03 +0000

    another try

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './mama-cafe',
+  base: 'https://dabami.github.io/mama-cafe/',
 })
